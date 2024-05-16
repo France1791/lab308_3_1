472b0792c7f799a4318c2523002659dc13e42c5e
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-05-16 16:25:09 -0400

    created arrays for data table

--- app.js
+++ app.js
@@ -42,4 +42,18 @@ function primeNum(n){
 }
 primeNum(100)
 
+// the CSV file
+let row = [];
+let peopleData = [];
 
+function isBetter(str){
+    row = str.split('\n');
+    console.log(row);
+
+    for(i = 0; i<row.length; i++){
+        person=row[i].split(',');
+        console.log(person);
+    }
+}
+
+isBetter("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26")
